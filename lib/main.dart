0a32d19ccb4cfbaba722585692dfbc4b3f5ea1a3
massil30 <massilattaf04@gmail.com>
2024-08-26 20:29:20 +0100

    new version

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 var x;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+//main
   final SharedPreferencesWithCache prefsWithCache =
       await SharedPreferencesWithCache.create(
     cacheOptions: const SharedPreferencesWithCacheOptions(
